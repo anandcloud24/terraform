3568d3f1d74061c898d5f700ab78c948d4b61e57
anandcloud24 <thommandruanand54@gmail.com>
2025-10-28 14:10:35 +0530

    day1

---
+++ day1/main.tf
@@ -0,0 +1,14 @@
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "vpc_terr"
+  }
+}
+
+resource "aws_subnet" "name" {
+    vpc_id = aws_vpc.vpc.id
+    cidr_block = "10.0.0.0/24"
+    tags = {
+    Name = "subnet1"
+    }
+}
