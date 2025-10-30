0ced242f20e3446895cbddb1223e37111f1f1f87
anandcloud24 <thommandruanand54@gmail.com>
2025-10-30 16:48:03 +0530

    day4

--- day4-locked-dynamodb/backend.tf
+++ day4-locked-dynamodb/backend.tf
@@ -1,10 +1,11 @@
 terraform {
   backend "s3" {
-    bucket              = "cloudanand.in"
-    key                 = "terraform.tfstate"
-    region              = "us-east-1"
-    s3_use_lockfile     = true
-    encrypt             = true
+    bucket = "cloudanand.in"
+    key    = "terraform.tfstate"
+    #use_lockfile = true # to use s3 native locking 1.19 version above
+    region = "us-east-1"
+    dynamodb_table = "Anand" # any version we can use dynamodb locking 
+    encrypt = true
   }
 }
 
