d7743348db832c4c5a940a51a2bfc75e314659f6
anandcloud24 <thommandruanand54@gmail.com>
2025-11-01 13:13:04 +0530

    day6

--- day6-importing/main.tf
+++ day6-importing/main.tf
@@ -7,3 +7,6 @@ resource "aws_instance" "name" {
     }
 
   }
+#terraform import aws_instance.name i-094c702f6ef80b351
+# the import will be manual creating instance in aws and as is of amiid,region,instance_type,tags 
+#give the information created instance data in vs code in top command to run in vs code <in the command place instance id>
